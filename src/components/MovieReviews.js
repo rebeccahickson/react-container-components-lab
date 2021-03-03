7d0c837669bfbca3a3c3652ef63e0d9966b4201f
Rebecca Hickson <rebecca.l.hickson@gmail.com>
2021-03-03 14:19:51 -0500

    commit tree 49ee6f8df37c71f4bc0ebe2da385b4521cd28da7

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,21 @@
-// Code MovieReviews Here
+import React from "react";
+
+const List = (review) => {
+  return (
+    <ul className="review">
+      <li>Title: {review.display_title}</li>
+      <li>Rating: {review.mpaa_rating}</li>
+      <li>Headline: {review.headline}</li>
+      <li>Summary: {review.summary_short}</li>
+    </ul>
+  );
+};
+
+const MovieReviews = ({ reviews }) => (
+  <div className="review-list">{reviews.map(List)}</div>
+);
+MovieReviews.defaultProps = {
+  reviews: [],
+};
+
+export default MovieReviews;
--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -1,9 +1,44 @@
-import React, { Component } from 'react';
-import 'isomorphic-fetch';
-import MovieReviews from './MovieReviews'
+import React, { Component } from "react";
+import "isomorphic-fetch";
+import MovieReviews from "./MovieReviews";
 
-const NYT_API_KEY = 'dGpQ5OmGP2SgfvZimlpCUoF4iOag9qzZ';
-const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?'
-            + `api-key=${NYT_API_KEY}`;
+const NYT_API_KEY = "dGpQ5OmGP2SgfvZimlpCUoF4iOag9qzZ";
+const URL =
+  "https://api.nytimes.com/svc/movies/v2/reviews/search.json?" +
+  `api-key=${NYT_API_KEY}`;
 
-// Code SearchableMovieReviewsContainer Here
+export default class SearchableMovieReviewsContainer extends Component {
+  state = {
+    searchTerm: "",
+    reviews: [],
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    fetch(URL + `&query=${this.state.searchTerm}`)
+      .then((res) => res.json())
+      .then((data) =>
+        this.setState({
+          reviews: data.results,
+        })
+      );
+  };
+
+  handleChange = (e) => {
+    this.setState({
+      searchTerm: e.target.value,
+    });
+  };
+
+  render() {
+    return (
+      <div className="searchable-movie-reviews">
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" onChange={this.handleChange} />
+          <input type="submit" />
+        </form>
+        <MovieReviews reviews={this.state.reviews} />
+      </div>
+    );
+  }
+}
